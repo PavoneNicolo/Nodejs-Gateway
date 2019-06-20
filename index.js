4ba8e1ff55dd9b0715eacfaa2b6ad9278a79c42f
Nicolò Pavone <nicolo.pavone@tecnicosuperiorekennedy.it>
2019-06-20 22:15:38 +0200

    Aggiunta init per MQTT. Nell'index ordinata per priorità init seriale/MQTT.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const serialportLibEvents = require('./lib/serialport-library-events/serial-events');
-const config = require('./lib/config/init-config');
+const serialService = require('./lib/services/serial-service');
+const initConfig = require('./lib/config/init-config');
 
-const portName = process.argv[2];// get port name from the command line
-const parser = config.getParser(); // make a new parser to read ASCII lines
-const serialOptions = {baudRate: 9600, dataBits: 8, stopBits: 2, parity: 'odd', autoOpen: false, parser: parser};
-const serialPort = config.portConfig(portName, serialOptions);
+// INIT
 
-// start serialport events listeners
-serialportLibEvents.open(serialPort);
-serialportLibEvents.receive(serialPort);
-serialportLibEvents.close(serialPort);
-serialportLibEvents.error(serialPort);
+let serialPort;
+let mqttClient;
 
-//myPort.write("ciao");
-
+// find USB serial port
+initConfig.serial.getPortName()
+    // configure serial port
+    .then((port) => {
+        console.log('Port ' + port + ' selected...');
+        const serialOptions = {baudRate: 9600, dataBits: 8, stopBits: 2, parity: 'odd', autoOpen: false};
+        serialPort = initConfig.serial.portConfig(port, serialOptions);
+    })
+    // MQTT INIT
+    .then(() => {
+        const brokerURL = 'mqtt://test.mosquitto.org';
+        const options = {};
+        mqttClient = initConfig.MQTT.connect(brokerURL, options);
+    })
+    // start serial port events listeners
+    .then(() => {
+        serialService.open(serialPort);
+        serialService.receive(serialPort, mqttClient);
+        serialService.close(serialPort);
+        serialService.error(serialPort);
+    })
+    .then(() => {
+        mqttClient.on('message', function (topic, message) {
+            // giro il comando in seriale al device giusto
+            // serialService.send(serialPort, message);
+            console.log(message.toString());
+        })
+    })
+    .catch((e) => {
+        console.log(e);
+    });
