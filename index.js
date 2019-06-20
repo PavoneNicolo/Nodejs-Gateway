8a06e772026ed183d1c7a1360a4dddf4779acb29
Nicolò Pavone <nicolo.pavone@tecnicosuperiorekennedy.it>
2019-06-20 11:38:20 +0200

    Ricevo i messaggi correttamente sulla seriale

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-//const serialport = require('serialport');// include the library
-//const SerialPort = require('@serialport/stream');
+const serialport = require('serialport');// include the library
+const SerialPort = require('@serialport/stream');
 const serialportLibEvents = require('./lib/serialport-library-events/serial-events');
 const config = require('./lib/config/init-config');
 
 const portName = process.argv[2];// get port name from the command line
-const serialOptions = {baudRate: 9600, dataBits: 8, stopBits: 2, parity: 'odd'};
-
-//const myPort = new SerialPort(portName, serialOptions);
-const myPort = config.portConfig(portName, serialOptions);
-//const Readline = SerialPort.parsers.Readline; // make instance of Readline parser
-//const parser = new Readline(); // make a new parser to read ASCII lines
 const parser = config.getParser(); // make a new parser to read ASCII lines
-
-myPort.pipe(parser); // pipe the serial stream to the parser
+const serialOptions = {baudRate: 9600, dataBits: 8, stopBits: 2, parity: 'odd', autoOpen: false, parser: parser};
+const serialPort = config.portConfig(portName, serialOptions);
 
 // start serialport events listeners
-serialportLibEvents.open(myPort);
-serialportLibEvents.receive(parser);
-serialportLibEvents.close(myPort);
-serialportLibEvents.error(myPort);
+serialportLibEvents.open(serialPort);
+serialportLibEvents.receive(serialPort);
+serialportLibEvents.close(serialPort);
+serialportLibEvents.error(serialPort);
 
 //myPort.write("ciao");
 
